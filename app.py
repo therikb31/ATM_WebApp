50b3297c49d906d665c1cb4eb1ec75c8d02105af
therikb31 <therikb31@gmail.com>
2021-08-21 19:18:21 +0530

    Added template create.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flask.templating import render_template
 from werkzeug.utils import redirect
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("home.html")
+
+@app.route("/login",methods=['GET','POST'])
+def callLogin():
+    if request.method == 'POST':
+        accountNumber = request.form['accountNumber']
+        pin = request.form['pin']
+    return render_template("login.html")
+
+@app.route("/create",methods=['GET','POST'])
+def createAccount():
+    return render_template("createAccount.html")
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
