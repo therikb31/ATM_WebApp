6a8f98107a2a356547ed314bb51cab3b1cb0f64c
therikb31 <therikb31@gmail.com>
2021-08-21 20:03:48 +0530

    minor Changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,request
+from flask import Flask, request
 from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
@@ -8,16 +9,19 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
-@app.route("/login",methods=['GET','POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def callLogin():
     if request.method == 'POST':
         accountNumber = request.form['accountNumber']
         pin = request.form['pin']
     return render_template("login.html")
 
-@app.route("/create",methods=['GET','POST'])
+
+@app.route("/create", methods=['GET', 'POST'])
 def createAccount():
     return render_template("createAccount.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True,port=8000)
+    app.run(debug=True, port=8000)
